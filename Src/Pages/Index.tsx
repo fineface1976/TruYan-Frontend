bf18c469189b7a01b6b289e414e430f3ff3a650a
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-26 16:59:05 +0100

    Create Index.tsx

---
+++ Src/Pages/Index.tsx
@@ -0,0 +1,10 @@
+import WalletConnect from '../components/wallet/WalletConnect';
+
+   export default function Home() {
+     return (
+       <div>
+         <h1>Welcome to TruYan!</h1>
+         <WalletConnect />
+       </div>
+     );
+   }
